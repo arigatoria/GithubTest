0c46537637ddf04a323edce41c9545d289789955
Ivan MSI <arigatory@gmail.com>
2022-04-20 19:31:40 +0300

    Add project files.

---
+++ GithubTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
